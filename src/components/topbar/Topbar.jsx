8f571fb6556abfe7efe4bdf3199fa020b1397d09
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2023-03-26 22:57:27 +0100

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Topbar.css'
+
+function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarwrapper">
+        <div className="topLeft">
+          <span classname="logo">kingmuffyadmin</span>
+        </div>
+        <div className="topRight">Right</div>
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
